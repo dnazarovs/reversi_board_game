eea2a41ce0253af6b6ddef9a1223c0c0a38ae0ab
daniils.nazarovs <nazarovs.daniils@gmail.com>
2023-04-11 22:53:30 +0300

    adding comments, fixing bug

--- app/javascript/controllers/reversi_controller.js
+++ app/javascript/controllers/reversi_controller.js
@@ -3,20 +3,22 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="reversi"
 export default class extends Controller {
 
+  // sets all targets to be available in the controller
   static targets = ["board", "startGame", "resetGame", "counter", "endGame"];
   connect() {
   }
 
+  // method to start the game when the user clicks on the start button and sets the first player
   startGame(event) {
-    const firstPlayer = event.target.dataset.player;
-    this.startGameTarget.style.display = 'none';
-    this.humanColor = firstPlayer === 'human' ? 'black' : 'white';
-    this.computerColor = firstPlayer === 'human' ? 'white' : 'black';
+    const firstPlayer = event.target.dataset.player; // gets the data-player attribute from the button
+    this.startGameTarget.style.display = 'none';  // hides the start button
+    this.humanColor = firstPlayer === 'human' ? 'black' : 'white'; // sets the color of the human player
+    this.computerColor = firstPlayer === 'human' ? 'white' : 'black'; // sets the color of the computer player
     this.initialiseBoard();
-    this.counterTarget.style.display = '';
-    this.resetGameTarget.style.display = 'block';
+    this.counterTarget.style.display = ''; // shows the counter
+    this.resetGameTarget.style.display = 'block'; // shows the reset button
     if (firstPlayer === 'computer') {
-      this.computerMove();
+      this.computerMove(); // if the computer is the first player, it makes the first move
     }
   }
 
@@ -24,9 +26,10 @@ export default class extends Controller {
     window.location.reload();
   }
 
+    // method to initialise the board
   initialiseBoard() {
     const boardSize = 8;
-    this.board = new Array(boardSize).fill(null).map(() => new Array(boardSize).fill(null));
+    this.board = new Array(boardSize).fill(null).map(() => new Array(boardSize).fill(null)); // creates a 2D array of 8x8
     const boardElement = document.createElement('div');
     boardElement.classList.add('board');
 
@@ -36,59 +39,67 @@ export default class extends Controller {
         cellElement.classList.add('cell');
         cellElement.dataset.row = row;
         cellElement.dataset.col = col;
-        cellElement.addEventListener('click', this.makeMove.bind(this));
+        cellElement.addEventListener('click', this.makeMove.bind(this)); // adds an event listener to each cell
         boardElement.appendChild(cellElement);
       }
     }
     this.boardTarget.appendChild(boardElement);
 
+    // sets the initial pieces
     this.setPiece(3, 3, 'white');
     this.setPiece(3, 4, 'black');
     this.setPiece(4, 3, 'black');
     this.setPiece(4, 4, 'white');
-    this.updatePieceCount();
-    this.highlightValidMoves();
+
+    this.updatePieceCount(); // updates the counter
+    this.highlightValidMoves(); // highlights the valid moves for user
     this.initialBoardState = JSON.parse(JSON.stringify(this.board));
   }
 
+  // gets cell element from row and column
   getCellElement(row, col) {
     return this.boardTarget.querySelector(`[data-row="${row}"][data-col="${col}"]`);
   }
 
+  // sets the piece on the board
   setPiece( row, col, color ) {
     const cell = this.getCellElement(row, col);
     const piece = document.createElement('div');
-    piece.classList.add('piece', color);
+    piece.classList.add('piece', color); // adds the class piece with color of player
     cell.appendChild(piece);
     this.board[row][col] = color;
   }
 
+  // deletes the piece from the board
   deletePiece( row, col ) {
     const cell = this.getCellElement(row, col);
     cell.removeChild(cell.firstChild);
     this.board[row][col] = null;
   }
 
+  // main method to make a move is executed when the user clicks on a cell
   makeMove(event) {
     if (this.isComputerMoving) {
       return; // Do not allow user to make a move if the computer is currently moving
     }
 
-    const targetElement = event.target.classList.contains('piece') ? event.target.parentElement : event.target;
+    const targetElement = event.target.classList.contains('piece') ? event.target.parentElement : event.target; // if user tries to click on the piece, it will get the parent element
     const row = parseInt(targetElement.dataset.row);
     const col = parseInt(targetElement.dataset.col);
 
-    if (!this.board[row][col] && this.isValidMove(row, col, this.humanColor)) {
+    if (!this.board[row][col] && this.isValidMove(row, col, this.humanColor)) { // checks if the cell is empty and if the move is valid
       this.setPiece(row, col, this.humanColor);
       this.flipPieces(row, col, this.humanColor);
       this.highlightValidMoves();
-    }
-    if (this.hasValidMoves(this.humanColor)) {
+      this.isComputerMoving = true; // sets the flag to true so computer will make a move next
+      setTimeout(() => this.computerMove(), 500); // waits for 500ms before making the computer move and calls the it
+    }else if (!this.hasValidMoves(this.humanColor)) { // if the user does not have any valid moves, it will make another move for the computer
       this.isComputerMoving = true;
       setTimeout(() => this.computerMove(), 500);
     }
   }
 
+  // method to check if the move is valid
   isValidMove(row, col, color) {
     const directions = [
       [-1, -1], [-1, 0], [-1, 1],
@@ -96,22 +107,22 @@ export default class extends Controller {
       [1, -1], [1, 0], [1, 1],
     ];
 
-    for (const [dx, dy] of directions) {
+    for (const [dx, dy] of directions) { // loops through all the directions
       let x = row + dx;
       let y = col + dy;
-      let foundOpponent = false;
+      let foundOpponent = false; // flag to check if the opponent piece is found
 
-      while (x >= 0 && x < 8 && y >= 0 && y < 8) {
+      while (x >= 0 && x < 8 && y >= 0 && y < 8) { // checks if the move is within the board
         if (this.board[x][y] === null) {
-          break;
+          break; // if the cell is empty, it will break the loop
         }
 
         if (this.board[x][y] !== color) {
-          foundOpponent = true;
+          foundOpponent = true; // if the cell is not empty and the color is not the same as the current player, it will set the flag to true
         } else if (foundOpponent) {
-          return true;
+          return true; // if the flag is true, it will return true
         } else {
-          break;
+          break; // if the flag is false, it will break the loop
         }
 
         x += dx;
@@ -122,6 +133,7 @@ export default class extends Controller {
     return false;
   }
 
+  // flip the pieces
   flipPieces(row, col, color) {
     const directions = [
       [-1, -1], [-1, 0], [-1, 1],
@@ -129,10 +141,10 @@ export default class extends Controller {
       [1, -1], [1, 0], [1, 1],
     ];
 
-    for (const [dx, dy] of directions) {
+    for (const [dx, dy] of directions) { // loops through all the directions
       let x = row + dx;
       let y = col + dy;
-      let toFlip = [];
+      let toFlip = []; // array to store the pieces to flip
 
       while (x >= 0 && x < 8 && y >= 0 && y < 8) {
         if (this.board[x][y] === null) {
@@ -140,11 +152,11 @@ export default class extends Controller {
         }
 
         if (this.board[x][y] !== color) {
-          toFlip.push([x, y]);
+          toFlip.push([x, y]); // adds the piece to the array
         } else {
-          for (const [fx, fy] of toFlip) {
-            this.deletePiece(fx, fy);
-            this.setPiece(fx, fy, color);
+          for (const [fx, fy] of toFlip) { // loops through the array and flips the pieces
+            this.deletePiece(fx, fy); // deletes the previous piece
+            this.setPiece(fx, fy, color); // sets the new piece
           }
           break;
         }
@@ -153,9 +165,10 @@ export default class extends Controller {
         y += dy;
       }
     }
-    this.updatePieceCount();
+    this.updatePieceCount(); // updates the counter
   }
 
+  // highlights the valid moves for the user
   highlightValidMoves() {
     const validMoveClass = 'valid-move';
     for (let row = 0; row < 8; row++) {
@@ -169,21 +182,28 @@ export default class extends Controller {
     }
   }
 
+  // makes the computer move
   async computerMove() {
-    const move = await this.getComputerMove(this.board, this.computerColor);
-    if (!move.pass) {
+    const move = await this.getComputerMove(this.board, this.computerColor); // gets row and col from backend
+    if (!move.pass) { // if returned value is not pass, it will make the move
       const { row, col } = move;
       this.setPiece(row, col, this.computerColor);
       this.flipPieces(row, col, this.computerColor);
     }
-    this.highlightValidMoves();
-    this.isComputerMoving = false;
+    if (this.hasValidMoves(this.humanColor)) { // checks if the user has any valid moves
+      this.isComputerMoving = false; // and only then tells that the computer has finished moving
+      this.highlightValidMoves(); // highlights the valid moves for the user
+    } else {
+      setTimeout(() => this.computerMove(), 500); // if the user does not have any valid moves, it will make another move for the computer
+    }
   }
 
+  // gets the computer move from the backend
   async getComputerMove(board, color) {
-    const gameId = this.element.dataset.gameId;
-    const sanitizedBoard = board.map(row => row.map(cell => cell === null ? '.' : cell));
+    const gameId = this.element.dataset.gameId; // gets the game id from the data attribute
+    const sanitizedBoard = board.map(row => row.map(cell => cell === null ? '.' : cell)); // sanitizes the board replaces null with .
 
+    // Ajax request to get the computer move
     const response = await fetch(`/games/${gameId}/ai_move`, {
       method: 'POST',
       headers: {
@@ -203,6 +223,7 @@ export default class extends Controller {
 
     const data = await response.json();
 
+    // if the returned value is null, it will return pass else it will return the row and col
     if (data.move === null) {
       return { pass: true };
     } else {
@@ -210,6 +231,7 @@ export default class extends Controller {
     }
   }
 
+  // checks if the player has any valid moves
   hasValidMoves(color) {
     for (let row = 0; row < 8; row++) {
       for (let col = 0; col < 8; col++) {
@@ -221,6 +243,7 @@ export default class extends Controller {
     return false;
   }
 
+  // updates the counter and ends the game if there are no more valid moves or the board is full
   updatePieceCount() {
     const blackScoreElement = this.element.querySelector('#black-score');
     const whiteScoreElement = this.element.querySelector('#white-score');
@@ -234,15 +257,18 @@ export default class extends Controller {
     blackScoreElement.textContent = blackScore;
     whiteScoreElement.textContent = whiteScore;
 
-    if (blackScore + whiteScore === 64 || (!this.hasValidMoves(this.humanColor) && !this.hasValidMoves(this.humanColor))) {
+    if (blackScore + whiteScore === 64 || (!this.hasValidMoves(this.humanColor) && !this.hasValidMoves(this.computerColor))) {
       const winner = blackScore > whiteScore ? 'Black' : (blackScore < whiteScore ? 'White' : null);
-      this.showEndGame(winner);
+        const winnerScore = blackScore > whiteScore ? blackScore : whiteScore;
+        const loserScore = blackScore > whiteScore ? whiteScore : blackScore;
+      this.showEndGame(winner, winnerScore, loserScore); // shows the end game message
     }
   }
 
-  showEndGame(winner) {
+  // shows the end game message
+  showEndGame(winner, winnerScore, loserScore) {
     const endGameMessage = this.endGameTarget.querySelector(".end-game-message");
-    endGameMessage.textContent = winner ? `Player ${winner} wins!` : "It's a draw!";
-    this.endGameTarget.style.display = "block";
+    endGameMessage.textContent = winner ? `Player ${winner} wins! with score: ${winnerScore}:${loserScore}` : "It's a draw!";
+    this.endGameTarget.style.display = "block"; // shows the end game message
   }
 }
--- lib/reversi_ai.rb
+++ lib/reversi_ai.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
-# Creating ReversiAi class
+# reversi_ai.rb
 class ReversiAi
+  # Creating DIRECTIONS constant as possible directions for a move
   DIRECTIONS = [
     [-1, -1], [-1, 0], [-1, 1],
     [0, -1],           [0, 1],
     [1, -1], [1, 0], [1, 1]
   ].freeze
 
+  # Creating initialize method, which takes in board, color, and difficulty
   def initialize(board, color, difficulty)
     @board = board
     @color = color
@@ -21,6 +23,7 @@ class ReversiAi
 
   end
 
+  # Move method which returns the best move for the AI to front end.
   def move
     best_move = alpha_beta(@board, @color, @difficulty, -Float::INFINITY, Float::INFINITY).last
     if best_move.nil?
@@ -34,28 +37,30 @@ class ReversiAi
 
   private
 
+  # Alpha beta algorithm to determine the best move for the AI
   def alpha_beta(board, color, depth, alpha, beta)
-    return [evaluate(board, color), nil] if depth.zero? || game_over?(board)
+    return [evaluate(board, color), nil] if depth.zero? || game_over?(board) # Return the score if the game is over or the depth is 0
 
     best_move = nil
-    moves = valid_moves(board, color)
+    moves = valid_moves(board, color) # Get all valid moves for the current player at current board state
 
     moves.each do |move|
       new_board = apply_move(board, move, color)
       opponent = color == 'white' ? 'black' : 'white'
       score = -alpha_beta(new_board, opponent, depth - 1, -beta, -alpha).first
 
-      if score > alpha
+      if score > alpha # If the score is greater than alpha, update alpha and best move
         alpha = score
         best_move = move
       end
 
-      break if alpha >= beta
+      break if alpha >= beta # If alpha is greater than or equal to beta, break the loop
     end
 
-    [alpha, best_move]
+    [alpha, best_move] # Return the score and best move
   end
 
+  # Evaluate the board state and return the score
   def evaluate(board, color)
     opponent = color == 'white' ? 'black' : 'white'
     my_pieces = board.flatten.count { |piece| piece == color }
@@ -63,21 +68,23 @@ class ReversiAi
     my_pieces - opponent_pieces
   end
 
+  # Check if the game is over
   def game_over?(board)
     valid_moves(board, 'white').empty? && valid_moves(board, 'black').empty?
   end
 
+  # Get all valid moves for the current player at current board state
   def valid_moves(board, color)
     opponent = color == 'white' ? 'black' : 'white'
     moves = []
 
     board.each_with_index do |row, row_idx|
-      row.each_with_index do |cell, col_idx|
-        next unless cell == '.'
+      row.each_with_index do |cell, col_idx| # Iterate through each cell in the board
+        next unless cell == '.'              # Skip the cell if it is not empty
 
-        DIRECTIONS.each do |direction|
+        DIRECTIONS.each do |direction| # Iterate through each direction
           if valid_move?(board, row_idx, col_idx, color, opponent, direction)
-            moves << [row_idx, col_idx]
+            moves << [row_idx, col_idx]    # Add the move to the moves array if it is a valid move
             break
           end
         end
@@ -86,56 +93,58 @@ class ReversiAi
     moves
   end
 
+  # Check if the move is valid
   def valid_move?(board, row, col, color, opponent, direction)
     row_step, col_step = direction
     row += row_step
     col += col_step
 
-    return false if !in_bounds?(row, col) || board[row][col] != opponent
+    return false if !in_bounds?(row, col) || board[row][col] != opponent # Return false if the move is not in bounds or the cell is not the opponent's piece
 
-    loop do
+    loop do # Loop through the board until the move is not valid
       row += row_step
-      col += col_step
-      return false unless in_bounds?(row, col)
+      col += col_step # Increment the row and column by the direction
+      return false unless in_bounds?(row, col) # Return false if the move is not in bounds
 
-      return true if board[row][col] == color
-      return false if board[row][col] == '.'
+      return true if board[row][col] == color # Return true if the cell is the current player's piece
+      return false if board[row][col] == '.' # Return false if the cell is empty
     end
   end
 
+  # Check if the move is in bounds
   def in_bounds?(row, col)
     row.between?(0, 7) && col.between?(0, 7)
   end
 
+  # Apply the move to the board
   def apply_move(board, move, color)
-    new_board = Marshal.load(Marshal.dump(board))
+    new_board = Marshal.load(Marshal.dump(board)) # Make a copy with new object array
     row, col = move
-    new_board[row][col] = color
+    new_board[row][col] = color # Set the cell to the current player's color
 
-    DIRECTIONS.each do |direction|
-      apply_move_direction(new_board, row, col, color, direction)
+    DIRECTIONS.each do |direction| # Iterate through each direction
+      apply_move_direction(new_board, row, col, color, direction) # Apply the move in the direction
     end
-
-    new_board
   end
 
+  # Apply the move in the direction
   def apply_move_direction(board, row, col, color, direction)
     row_step, col_step = direction
     row += row_step
     col += col_step
 
     opponent = color == 'white' ? 'black' : 'white'
-    cells_to_flip = []
+    cells_to_flip = [] # Array to store the cells to flip
 
-    while in_bounds?(row, col) && board[row][col] == opponent
-      cells_to_flip << [row, col]
+    while in_bounds?(row, col) && board[row][col] == opponent # Loop through the board until the move is not valid
+      cells_to_flip << [row, col] # Add the cell to the cells to flip array
       row += row_step
       col += col_step
     end
 
-    if in_bounds?(row, col) && board[row][col] == color
-      cells_to_flip.each do |flip_row, flip_col|
-        board[flip_row][flip_col] = color
+    if in_bounds?(row, col) && board[row][col] == color # If the cell is the current player's piece, flip the cells
+      cells_to_flip.each do |flip_row, flip_col| # Iterate through each cell to flip
+        board[flip_row][flip_col] = color # Flip the cell
       end
     end
   end
